7da28833ef0ce9e253709d98defb115b38ad5f29
jack.guo <ext.jack.guo@ef.com>
2019-05-30 13:57:34 +0800

    .

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,3 +1,4 @@
-docker login --username=1223984269@qq.com --password=123abc123 registry.cn-hangzhou.aliyuncs.com
+docker login --username=1223984269@qq.com --password=123abc123 registry.cn-qingdao.aliyuncs.com
 docker tag imageservice registry.cn-qingdao.aliyuncs.com/aliyun-jack/imageservice:2.1
 docker push registry.cn-qingdao.aliyuncs.com/aliyun-jack/imageservice:2.1
+
